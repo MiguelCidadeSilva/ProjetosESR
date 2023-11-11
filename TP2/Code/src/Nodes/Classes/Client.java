66f6060be63d29f8f087ccbc3780801ebdad0545
MiguelCidadeSilva <mikefsi@hotmail.com>
2023-11-11 16:14:43 +0000

    Code to run clients.

--- TP2/Code/src/Nodes/Classes/Client.java
+++ TP2/Code/src/Nodes/Classes/Client.java
@@ -1,4 +1,99 @@
 package Nodes.Classes;
 
+import Nodes.Utils.Cods;
+import Nodes.Utils.Debug;
+import Protocols.ProtocolBuildTree;
+import Protocols.ProtocolStartStreaming;
+import Protocols.ProtocolTransferContent;
+
+import java.io.*;
+import java.net.*;
+import java.util.List;
+
 public class Client {
+    private final InetAddress ipclient;
+    private final InetAddress ipneighbour;
+
+    public Client(InetAddress ipclient, InetAddress ipneighbour) {
+        this.ipclient = ipclient;
+        this.ipneighbour = ipneighbour;
+    }
+
+    public void start(String resource) throws IOException {
+        Debug.printTask("Inicio de criação da árvore.");
+        byte tree = requestTree(resource);
+        Debug.printTask("Árvore Criada com sucesso");
+        switch (tree) {
+            case 0: //loop
+                Debug.printTask("Erro, a rede entrou em loop.");
+                break;
+            case 1: //found
+                Debug.printTask("Recurso encontrado");
+                //startStreaming
+                Debug.printTask("Iniciação do Streaming");
+                startStreaming(resource);
+                Debug.printTask("Streaming");
+                getContent();
+                Debug.printTask("Fim de Streaming");
+                break;
+            case 2: //not found
+                Debug.printTask("Recurso não encontrado");
+                break;
+        }
+    }
+
+    private byte requestTree(String resource) throws IOException {
+        Debug.printTask("Criação do socket TCP");
+        try (
+                Socket socket = new Socket(ipneighbour, Cods.portSOConnections);
+                OutputStream outputStream = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(outputStream);
+                InputStream inputStream = socket.getInputStream();
+                DataInputStream dis = new DataInputStream(inputStream)
+        ) {
+            Debug.printTask("Encapsulamento dos dados a enviar.");
+            ProtocolBuildTree.encapsulateAsk(ipclient, resource, dos);
+            Debug.printTask("Desencapsulamento da resposta.");
+            // Receive the response from the server
+            byte res = ProtocolBuildTree.decapsulateAnswer(dis);
+            Debug.printTask("Resposta desencapsulada");
+            return res;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private void startStreaming(String resource){
+        Debug.printTask("Criação do socket TCP");
+        try (
+                Socket socket = new Socket(ipneighbour, Cods.portStartStreaming);
+                OutputStream outputStream = socket.getOutputStream();
+                DataOutputStream dos = new DataOutputStream(outputStream);
+                InputStream inputStream = socket.getInputStream();
+        ) {
+            Debug.printTask("Encapsulamento dos dados.");
+            ProtocolStartStreaming.encapsulate(resource,dos);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    private List<DatagramPacket> getContent(){
+        Debug.printTask("Criação do socket UDP");
+        try (DatagramSocket socket = new DatagramSocket(Cods.portStreamingContent))
+        {
+            byte[] buffer = new byte[20000];
+            Debug.printTask("Receção de pacotes UDP.");
+            while (true) {
+                DatagramPacket receivePacket = new DatagramPacket(buffer, buffer.length);
+                socket.receive(receivePacket);
+                StreamingPacket packet = ProtocolTransferContent.decapsulate(receivePacket);
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
--- TP2/Code/src/Nodes/Execs/ClientExec.java
+++ TP2/Code/src/Nodes/Execs/ClientExec.java
@@ -1,4 +1,15 @@
 package Nodes.Execs;
 
+import Nodes.Classes.Client;
+
+import java.io.IOException;
+import java.net.InetAddress;
+
 public class ClientExec {
+    public static void main(String[] args) throws IOException {
+        Client c = new Client(InetAddress.getByName(args[0]),InetAddress.getByName(args[1]));
+        System.out.println("Client with ip = "+args[0]+" whose neighbour is "+ args[1]+ " created.");
+        System.out.println("Client starting...");
+        c.start(args[2]);
+    }
 }
