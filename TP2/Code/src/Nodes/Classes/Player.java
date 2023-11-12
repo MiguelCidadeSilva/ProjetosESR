407b5186bf4fbe9cda8b2b6c3029565765bd4cd3
zeisalone <jmbarbosa2002@gmail.com>
2023-11-12 17:20:25 +0000

    Updated Player

--- TP2/Code/src/Nodes/Classes/Player.java
+++ TP2/Code/src/Nodes/Classes/Player.java
@@ -1,9 +1,13 @@
 package Nodes.Classes;
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.IOException;
+import java.io.*;
+
 public class Player {
     JFrame player = new JFrame("Player");
     JButton setupButton = new JButton("Setup");
@@ -13,7 +17,12 @@ public class Player {
     JPanel mainPanel = new JPanel();
     JPanel buttonPanel = new JPanel();
     JLabel iconLabel = new JLabel();
-    ImageIcon icon;
+
+    byte[] buf;
+
+
+    Timer cTimer;
+    private Clip clip;
 
     public Player() {
         player.addWindowListener(new WindowAdapter() {
@@ -23,27 +32,77 @@ public class Player {
         });
 
         buttonPanel.setLayout(new GridLayout(1,0));
-        buttonPanel.add(setupButton);
+        // buttonPanel.add(setupButton);
         buttonPanel.add(playButton);
         buttonPanel.add(pauseButton);
         buttonPanel.add(tearButton);
+
+        playButton.addActionListener(new playButtonListener());
+        tearButton.addActionListener(new tearButtonListener());
+        pauseButton.addActionListener(new pauseButtonListener());
+
         /*
         setupButton.addActionListener(new setupButtonListener());
-        playButton.addActionListener(new playButtonListener());
-        pauseButton.addActionListener(new pauseButtonListener());
-        tearButton.addActionListener(new tearButtonListener());
-         */
 
+
+
+         */
+        iconLabel.setIcon(null);
         mainPanel.setLayout(null);
         mainPanel.add(iconLabel);
         mainPanel.add(buttonPanel);
-        iconLabel.setBounds(0,0,380,280);
-        buttonPanel.setBounds(0,280,380,50);
+        iconLabel.setBounds(0,0,680,400);
+        buttonPanel.setBounds(0,280,600,50);
 
         player.getContentPane().add(mainPanel, BorderLayout.CENTER);
-        player.setSize(new Dimension(390,370));
+        player.setSize(new Dimension(600,400));
         player.setVisible(true);
 
 
+        //allocate enough memory for the buffer used to receive data from the server
+        buf = new byte[15000];
+
+    }
+    class playButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+
+            System.out.println("Play Button pressed !");
+            cTimer.start();
+        }
+    }
+
+    class tearButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e){
+
+            System.out.println("Teardown Button pressed !");
+            //stop the timer
+            cTimer.stop();
+            //exit
+            System.exit(0);
+        }
+    }
+    class pauseButtonListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            System.out.println("Pause Button pressed !");
+            if (clip != null && clip.isRunning()) {
+                clip.stop();
+            }
+        }
+    }
+    public void updateFrame(byte[] frame) {
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        Image image = toolkit.createImage(frame, 0, frame.length);
+
+        //display the image as an ImageIcon object
+        ImageIcon icon = new ImageIcon(image);
+        iconLabel.setIcon(icon);
+    }
+
+    public void updateAudio(byte[] audio) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        InputStream inputStream = new ByteArrayInputStream(audio);
+        AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(inputStream);
+        clip = AudioSystem.getClip();
+        clip.open(audioInputStream);
+        clip.start();
     }
 }
