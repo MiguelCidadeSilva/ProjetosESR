a6a383099cf337009ad0ffb684f6a06aa386b4de
Miguel Cidade Silva <73348796+MiguelCidadeSilva@users.noreply.github.com>
2023-11-17 18:46:39 +0000

    Update Buffer.java
    Still needs testing.

--- TP2/Code/src/Nodes/Classes/Buffer.java
+++ TP2/Code/src/Nodes/Classes/Buffer.java
@@ -3,48 +3,65 @@ package Nodes.Classes;
 import Nodes.Utils.Cods;
 import Nodes.Utils.VideoExtractor;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Buffer {
-    private final Map<String, Map<Integer, List<byte[]>>> bufferVideo;
-    private final Map<String, Map<Integer, List<byte[]>>> bufferAudio;
+    private final Map<Integer, Set<Integer>> receivedAudio;
+    private final Map<Integer, Set<Integer>> receivedVideo;
+    private final Map<Integer, Integer> expectedAudio;
+    private final Map<Integer, Integer> expectedVideo;
+    private final Map<Integer, List<byte[]>> bufferVideo;
+    private final Map<Integer, List<byte[]>> bufferAudio;
     private final Player p;
+
     public Buffer(Player p) {
         this.p = p;
         bufferAudio = new HashMap<>();
         bufferVideo = new HashMap<>();
+        receivedAudio = new HashMap<>();
+        receivedVideo = new HashMap<>();
+        expectedAudio = new HashMap<>();
+        expectedVideo = new HashMap<>();
     }
 
     public void receive(StreamingPacket sp) {
-        if(sp.getType() == Cods.codAudio) {
-            if(!bufferAudio.containsKey(sp.getResource()))
-                bufferAudio.put(sp.getResource(),new HashMap<>());
-	    if(!bufferAudio.get(sp.getResource()).containsKey(sp.getSequenceNumber()))
-	  	bufferAudio.get(sp.getResource()).put(sp.getSequenceNumber(), new ArrayList<>());
-            bufferAudio.get(sp.getResource()).get(sp.getSequenceNumber()).add(sp.getContent());
-            if(sp.getNrPackets() == 1)
-            {
-                List<byte[]> bytes = bufferAudio.get(sp.getResource()).get(sp.getSequenceNumber());
-                bufferAudio.get(sp.getResource()).remove(sp.getSequenceNumber());
-                byte[] content = VideoExtractor.concatenateChunks(bytes);
-                p.updateAudio(content);
-            }
+        if (sp.getType() == Cods.codAudio) {
+            handlePacket(sp, bufferAudio, receivedAudio, expectedAudio, false);
+        } else if (sp.getType() == Cods.codVideo) {
+            handlePacket(sp, bufferVideo, receivedVideo, expectedVideo, true);
         }
-        else if(sp.getType() == Cods.codVideo) {
-            if(!bufferVideo.containsKey(sp.getResource()))
-                bufferVideo.put(sp.getResource(),new HashMap<>());
-	    if(!bufferVideo.get(sp.getResource()).containsKey(sp.getSequenceNumber()))
-                bufferVideo.get(sp.getResource()).put(sp.getSequenceNumber(), new ArrayList<>());
-            bufferVideo.get(sp.getResource()).get(sp.getSequenceNumber()).add(sp.getContent());
-            if(sp.getNrPackets() == 1)
-            {
-                List<byte[]> bytes = bufferVideo.get(sp.getResource()).get(sp.getSequenceNumber());
-                bufferVideo.get(sp.getResource()).remove(sp.getSequenceNumber());
+    }
+
+    private void handlePacket(StreamingPacket sp, Map<Integer, List<byte[]>> buffer, Map<Integer, Set<Integer>> receivedPackets, Map<Integer, Integer> expected, boolean control) {
+        int sequenceNumber = sp.getSequenceNumber();
+
+        if (!buffer.containsKey(sequenceNumber)) {
+            buffer.put(sequenceNumber, new ArrayList<>());
+            receivedPackets.put(sequenceNumber, new HashSet<>());
+            expected.put(sequenceNumber, sp.getNrPackets());
+        }
+
+        buffer.get(sequenceNumber).add(sp.getContent());
+        receivedPackets.get(sequenceNumber).add(sp.getNrPackets());
+
+        int lastExpected = expected.get(sequenceNumber);
+
+        if (lastExpected == sp.getNrPackets()) {
+            while (!receivedPackets.get(sequenceNumber).contains(lastExpected)) {
+                lastExpected--;
+            }
+            expected.put(sequenceNumber, lastExpected);
+
+            if (lastExpected == 0) {
+                List<byte[]> bytes = buffer.get(sequenceNumber);
+                buffer.remove(sequenceNumber);
                 byte[] content = VideoExtractor.concatenateChunks(bytes);
-                p.updateFrame(content);
+
+                if (control) {
+                    p.updateFrame(content);
+                } else {
+                    p.updateAudio(content);
+                }
             }
         }
     }
